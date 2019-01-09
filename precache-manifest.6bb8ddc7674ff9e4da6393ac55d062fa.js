9b5dcc432465d78cb8e81daa75971b090000e9c1
Febin John James <f@badla.io>
2019-01-09 23:29:23 +0530

    UI change

---
+++ precache-manifest.6bb8ddc7674ff9e4da6393ac55d062fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77ddd9c10d772cdf96e3",
+    "url": "/static/js/main.77ddd9c1.chunk.js"
+  },
+  {
+    "revision": "fb6302a65665b70bc871",
+    "url": "/static/js/1.fb6302a6.chunk.js"
+  },
+  {
+    "revision": "77ddd9c10d772cdf96e3",
+    "url": "/static/css/main.67ccc3c2.chunk.css"
+  },
+  {
+    "revision": "f1dff294427e09cccedde8ddcb4a4ffa",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52aa87db338da7d00597318c7eedc30b.js"
+  "/precache-manifest.6bb8ddc7674ff9e4da6393ac55d062fa.js"
 );
 
 workbox.clientsClaim();
